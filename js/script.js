a985119d541eab4516df67552daebed30334de87
Evheniy Kalinichenko <kalinichenko0997z@gmail.com>
2020-08-07 06:31:50 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const money = 500;
+const income = 10000;
+const addExpenses = 5000;
+const deposit = 100;
+const mission = "car";
+const period = 3;
+
+alert("Hello world");
+
+console.log("Hello world");
