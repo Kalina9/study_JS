019d3e006958233cfbd19a61d2eb6b727f774f62
Evheniy Kalinichenko <kalinichenko0997z@gmail.com>
2020-09-07 21:27:05 +0300

    Добваил анимацию

--- js/script.js
+++ js/script.js
@@ -1,55 +1,182 @@
 // Обработчик ожидает загрузгу только DOM дерева - DOMContentLoaded
-window.addEventListener('DOMContentLoaded', function () {
-    'use strict';
+window.addEventListener("DOMContentLoaded", function () {
+  "use strict";
 
-    // Таймер
-    function countTimer(deadline) {
-        let timerHours = document.querySelector('#timer-hours'),
-            timerMinutes = document.querySelector('#timer-minutes'),
-            timerSeconds = document.querySelector('#timer-seconds');
-        //    Условие , которое добавляет 0
-        let addZero = function (num) {
-            if (num <= 9) {
-                return "0" + num;
-            } else {
-                return num
-            }
-        };
+  // Таймер
+  function countTimer(deadline) {
+    let timerHours = document.querySelector("#timer-hours"),
+      timerMinutes = document.querySelector("#timer-minutes"),
+      timerSeconds = document.querySelector("#timer-seconds");
+    //    Условие , которое добавляет 0
+    let addZero = function (num) {
+      if (num <= 9) {
+        return "0" + num;
+      } else {
+        return num;
+      }
+    };
 
-        function getTimeRemaining() {
-            let dateStop = new Date(deadline).getTime(),
-                dateNow = new Date().getTime(),
-                timeRemaining = (dateStop - dateNow) / 1000,
-                seconds = Math.floor(timeRemaining % 60),
-                minutes = Math.floor((timeRemaining / 60) % 60),
-                hours = Math.floor(timeRemaining / 60 / 60);
-            return {
-                timeRemaining,
-                hours,
-                minutes,
-                seconds
-            };
-        }
-
-        function updateClock() {
-            let timer = getTimeRemaining();
-            timerHours.textContent = addZero(timer.hours);
-            timerMinutes.textContent = addZero(timer.minutes);
-            timerSeconds.textContent = addZero(timer.seconds);
-            // Если  событие прошло,  таймер  становится 00:00:00
-            if (timer.timeRemaining <= 0) {
-                timerHours.textContent = '00';
-                timerMinutes.textContent = "00";
-                timerSeconds.textContent = "00";
-                clearInterval(idInterval)
-            }
-        }
-        let idInterval = setInterval(updateClock, 1000)
-
-        updateClock()
-
-
+    function getTimeRemaining() {
+      let dateStop = new Date(deadline).getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining = (dateStop - dateNow) / 1000,
+        seconds = Math.floor(timeRemaining % 60),
+        minutes = Math.floor((timeRemaining / 60) % 60),
+        hours = Math.floor(timeRemaining / 60 / 60);
+      return {
+        timeRemaining,
+        hours,
+        minutes,
+        seconds,
+      };
     }
-    //countTimer();
-    countTimer(`7 september 2020`)
+
+    function updateClock() {
+      let timer = getTimeRemaining();
+      timerHours.textContent = addZero(timer.hours);
+      timerMinutes.textContent = addZero(timer.minutes);
+      timerSeconds.textContent = addZero(timer.seconds);
+      // Если  событие прошло,  таймер  становится 00:00:00
+      if (timer.timeRemaining <= 0) {
+        timerHours.textContent = "00";
+        timerMinutes.textContent = "00";
+        timerSeconds.textContent = "00";
+        clearInterval(idInterval);
+      }
+    }
+    let idInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+  }
+  //countTimer();
+  countTimer(`7 september 2020`);
+
+  // Меню
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector(".menu"),
+      menu = document.querySelector("menu");
+    // closeBtn = document.querySelector(".close-btn"),
+    // menuItems = menu.querySelectorAll("ul>li");
+    const handlerMenu = () => {
+      //   toggle - убирает или добавляет класс
+      menu.classList.toggle("active-menu");
+      // if (!menu.style.transform || menu.style.transform === 'translate(-100%)') {
+      //     menu.style.transform = 'translate(0)'
+      // } else {
+      //     menu.style.transform = 'translate(-100%)'
+      // }
+    };
+    //
+    menu.addEventListener("click", (event) => {
+      let target = event.target;
+      if (target.classList.contains("close-btn")) {
+        handlerMenu();
+      } else if (target.tagName == "A") {
+        handlerMenu();
+      }
+
+      console.log(target);
+    });
+
+    btnMenu.addEventListener("click", (event) => {
+      let target = event.target.closest(".menu");
+      if (!target) {
+        return;
+      } else {
+        handlerMenu();
+      }
+
+      console.log(target);
+    });
+    // closeBtn.addEventListener("click", handlerMenu);
+    //  menuItems.forEach((elem) => {
+    //    elem.addEventListener("click", handlerMenu);
+    //  });
+  };
+  toggleMenu();
+  // popup
+  const togglePopUp = () => {
+    const popup = document.querySelector(".popup"),
+      popupBtn = document.querySelectorAll(".popup-btn");
+
+    popupBtn.forEach((elem) => {
+      elem.addEventListener("click", () => {
+        popup.style.display = "block";
+      });
+    });
+
+    popup.addEventListener("click", (event) => {
+      let target = event.target;
+
+      if (target.classList.contains("popup-close")) {
+        popup.style.display = "none";
+      } else {
+        target = target.contains(".popup-content");
+        if (!target) {
+          popup.style.display = "none";
+        }
+      }
+
+      console.log(target);
+    });
+  };
+  togglePopUp();
+  // Анимация
+
+  let makeAnimate = () => {
+    let animate = (draw, duration) => {
+      let start = performance.now();
+      requestAnimationFrame(function animate(time) {
+        let timePassed = time - start;
+        if (timePassed > duration) timePassed = duration;
+        draw(timePassed);
+        if (timePassed < duration) {
+          requestAnimationFrame(animate);
+        }
+      });
+    };
+    let popupContent = document.querySelector(".popup-content"),
+      popupBtn = document.querySelectorAll(".popup-btn");
+
+    popupBtn.forEach((elem) => {
+      elem.addEventListener("click", () => {
+        animate(function (timePassed) {
+          popupContent.style.top = timePassed / 2 + "px";
+        }, 300);
+      });
+    });
+  };
+
+  makeAnimate();
+
+  // табы
+  const tabs = () => {
+    const tabHeader = document.querySelector(".service-header"),
+      tab = tabHeader.querySelectorAll(".service-header-tab"),
+      tabContent = document.querySelectorAll(".service-tab");
+    const toggleTabContent = (index) => {
+      for (let i = 0; i < tabContent.length; i++) {
+        if (index === i) {
+          tab[i].classList.add("active");
+          tabContent[i].classList.remove("d-none");
+        } else {
+          tab[i].classList.remove("active");
+          tabContent[i].classList.add("d-none");
+        }
+      }
+    };
+    tabHeader.addEventListener("click", (event) => {
+      let target = event.target;
+      target = target.closest(".service-header-tab");
+
+      if (target) {
+        tab.forEach((item, i) => {
+          if (item === target) {
+            toggleTabContent(i);
+          }
+        });
+      }
+    });
+  };
+  tabs();
 });
